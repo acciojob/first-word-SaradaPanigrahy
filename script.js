e7b724e2b75206580711d29ecdd8a3afbac5db16
SaradaPanigrahy <98700255+SaradaPanigrahy@users.noreply.github.com>
2025-09-19 19:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function firstWord(s) {
   // your code here
+	let ss=s.trim();
+	cost arr=ss.split(" ");
+	return arr[0];
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
